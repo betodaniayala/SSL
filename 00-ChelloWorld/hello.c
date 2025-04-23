d3827167312e47f2ff62255886cd6d7927ba0c58
Alberto Ayala <alberto@frba.utn.edu.ar>
2025-04-23 01:57:32 -0300

    correccion de hello.c

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -4,16 +4,7 @@
 
 int main(void){
 	
-	FILE * archivo= NULL;
-	
-	archivo = fopen("output.txt","w");
-	
 	printf(" Hola ALberto Daniel Ayala Cabrera! \n");
-	
-	fprintf(archivo, " Hola ALberto Daniel Ayala Cabrera! \n");
-	
-	fclose(archivo);
-	
 	
 	return(0);
 }
